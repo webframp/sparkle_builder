c7811fa55ce725262665977d461b6da13cfe685b
Chris Roberts <chrisroberts.code@gmail.com>
2014-08-01 10:38:45 -0700

    Initialize config data structure for hooks

--- config/initializers/builder_configuration.rb
+++ config/initializers/builder_configuration.rb
@@ -1,3 +1,19 @@
 unless(Rails.application.config.sparkle[:storage])
   Rails.application.config.sparkle[:storage] = {}.with_indifferent_access
+  Rails.application.config.sparkle[:builder] = {
+    :hooks => {
+      :create => {
+        :before => [],
+        :after => []
+      }.with_indifferent_access,
+      :update => {
+        :before => [],
+        :after => []
+      }.with_indifferent_access,
+      :destroy => {
+        :before => [],
+        :after => []
+      }.with_indifferent_access
+    }.with_indifferent_access
+  }.with_indifferent_access
 end
